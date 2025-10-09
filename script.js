b18a97da181680225eb76a315d5c23b10fa02e49
DROVE261 <barbourmersin@icloud.com>
2025-10-09 14:05:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+// الدخول
 document.getElementById("google-login").onclick = () => {
   document.getElementById("login-screen").classList.add("hidden");
   document.getElementById("main-content").classList.remove("hidden");
 };
-
+document.getElementById("twitter-login").onclick = () => {
+  document.getElementById("login-screen").classList.add("hidden");
+  document.getElementById("main-content").classList.remove("hidden");
+};
+document.getElementById("apple-login").onclick = () => {
+  document.getElementById("login-screen").classList.add("hidden");
+  document.getElementById("main-content").classList.remove("hidden");
+};
 document.getElementById("signup-btn").onclick = () => {
   alert("ميزة إنشاء الحساب قيد التطوير ❤️");
+  document.getElementById("login-screen").classList.add("hidden");
+  document.getElementById("main-content").classList.remove("hidden");
 };
 
+// تغيير اللغة
 function changeLang(lang) {
-  if (lang === "tr") document.getElementById("title").textContent = "Kuveyt Türk'e Hoş Geldiniz";
-  if (lang === "en") document.getElementById("title").textContent = "Welcome to Kuveyt Türk";
-  if (lang === "ar") document.getElementById("title").textContent = "مرحباً بك في Kuveyt Türk";
+  const title = document.getElementById("title");
+  if(lang==="ar") title.textContent="مرحباً بك في Kuveyt Türk";
+  if(lang==="tr") title.textContent="Kuveyt Türk'e Hoş Geldiniz";
+  if(lang==="en") title.textContent="Welcome to Kuveyt Türk";
 }
